3f01ace81e0b5341082ccb5a4786bb575299f393
Joris <jote0002@stud.kea.dk>
2024-09-29 10:02:01 +0200

    tilføjet anim. ændret farvekoder

--- src/js/database.js
+++
@@ -1,7 +0,0 @@
-const data = await fetch(`https://kxmqexnqozccamiawnbj.supabase.co/rest/v1/table1?`, {
-  method: "GET",
-  headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bXFleG5xb3pjY2FtaWF3bmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg3MDQyNjIsImV4cCI6MjAzNDI4MDI2Mn0.9bmPlpp_agUnjY_gAe2RymTKggwDxzy8JT-XLQNNhRA",
-  },
-}).then((res) => res.json());
-console.log(data);
---
+++ src/js/viewtransition.js
@@ -0,0 +1,25 @@
+// Function to check if element is in the viewport
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
+
+// Function to add a class when the element is in the viewport
+function addClassOnScroll(elements, className) {
+  window.addEventListener("scroll", function () {
+    elements.forEach((element) => {
+      if (isInViewport(element)) {
+        element.classList.add(className);
+      }
+    });
+  });
+}
+
+// Example Usage
+document.addEventListener("DOMContentLoaded", function () {
+  const myElements = document.querySelectorAll("p, .viewable, h2, h6, .profilepic"); // Get all <p> elements
+  const myClass = "in-viewport"; // Class to add
+
+  // Call the function and pass the NodeList
+  addClassOnScroll(myElements, myClass);
+});
